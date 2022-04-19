a3edc5bafacb072f25d989b3e9c4eb6c00c98340
Jens Fredskov <jsfr@users.noreply.github.com>
2022-04-19 16:43:57 +0200

    Label yabai signals to avoid adding double signals on restart of hammerspoon

--- Source/YabaiSpaces.spoon/signals.sh
+++ Source/YabaiSpaces.spoon/signals.sh
@@ -2,7 +2,7 @@
 
 yabai="$YABAI_PATH"
 
-$yabai -m signal --add event=application_launched    action="hs -A -c 'spoon.YabaiSpaces:update()'"
-$yabai -m signal --add event=application_deactivated action="hs -A -c 'spoon.YabaiSpaces:update()'"
-$yabai -m signal --add event=application_terminated  action="hs -A -c 'spoon.YabaiSpaces:update()'"
-$yabai -m signal --add event=space_changed           action="hs -A -c 'spoon.YabaiSpaces:update()'"
+$yabai -m signal --add label=hammerspoon_spaces_1 event=application_launched    action="hs -A -c 'spoon.YabaiSpaces:update()'"
+$yabai -m signal --add label=hammerspoon_spaces_2 event=application_deactivated action="hs -A -c 'spoon.YabaiSpaces:update()'"
+$yabai -m signal --add label=hammerspoon_spaces_3 event=application_terminated  action="hs -A -c 'spoon.YabaiSpaces:update()'"
+$yabai -m signal --add label=hammerspoon_spaces_4 event=space_changed           action="hs -A -c 'spoon.YabaiSpaces:update()'"
